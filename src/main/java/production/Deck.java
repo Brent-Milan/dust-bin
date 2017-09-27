0024ddba52d6982eadb591705f6f16b13040bcc9
Brent-Milan <Bmilan24@gmail.com>
2017-09-27 02:30:01 -0400

    new test in Deck passing for createSuit()

--- src/main/java/production/Deck.java
+++ src/main/java/production/Deck.java
@@ -1,11 +1,25 @@
 package production;
 
+import java.util.ArrayList;
+
 public class Deck {
 	
-	String[] cards;
+	Card card = new Card();
+	ArrayList<String> cards = new ArrayList<String>();
 
-	public String createCard() {
-		return "S2";
+	public void createSuit(String suit) {
+		String suitLetter = card.convertToString(suit);
+		int[] values = card.values;
+		for(int index = 0; index < values.length; index++) {
+			String value = card.convertToString(values[index]);
+			String card = suitLetter + value;
+			cards.add(card);
+		}
+		
+	}
+
+	public ArrayList<String> getCards() {
+		return cards;
 	}
 
 }
--- src/test/java/production/DeckTest.java
+++ src/test/java/production/DeckTest.java
@@ -2,12 +2,15 @@ package production;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class DeckTest {
 	
 	Deck underTest;
+	Card card = new Card();
 
 	@Before
 	public void setUp() throws Exception {
@@ -15,11 +18,32 @@ public class DeckTest {
 	}
 	
 	@Test
-	public void shouldReturnStringS2() {
-		String expected = "S2";
+	public void shouldReturnSuitOfSpades() {
+		ArrayList<String> expected = createSpades();
+		underTest.createSuit("Spades");
 		
-		assertEquals(expected, underTest.createCard());
+		assertEquals(expected, underTest.getCards());
 	}
+
+	private ArrayList<String> createSpades() {
+		ArrayList<String> spades = new ArrayList<String>();
+		spades.add("S2");
+		spades.add("S3");
+		spades.add("S4");
+		spades.add("S5");
+		spades.add("S6");
+		spades.add("S7");
+		spades.add("S8");
+		spades.add("S9");
+		spades.add("ST");
+		spades.add("SJ");
+		spades.add("SQ");
+		spades.add("SK");
+		spades.add("SA");
+		return spades;
+	}
+	
+	
 
 //	@Test
 //	public void shouldContainAllHeartCards() {
